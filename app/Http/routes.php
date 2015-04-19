c346760555f291a033ffd87f2f34a58bf7e937a1
Nguyễn Hà Bao Duy <nguyen.habaoduy@gmail.com>
2015-04-19 10:02:24 +0800

    Update
    Change GET to POST DELETE for remove images

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,7 @@ $router->resource('user', 'UserController');
 
 
 $router->post('/property/{property}/upload', 'PropertyController@upload');
-$router->get('/property/{property}/removeImages', 'PropertyController@removeAllImages');
+$router->delete('/property/{property}/removeImages', 'PropertyController@removeAllImages');
 
 $router->resource('agent.client', 'AgentClientController');
 
